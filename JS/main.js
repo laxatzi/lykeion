8528035bea3a8c8bcb5aa970222504c60d1b2807
laxatzi <laxatzi@yahoo.com>
2023-10-23 17:13:40 +0300

    Add active page js code in main.js

--- JS/main.js
+++ JS/main.js
@@ -33,29 +33,53 @@ window.onload = function () {
   };
 
   // Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+
+  //  Alternative topScroll function
+  // window.addEventListener("scroll", showScrollButton);
+
+  // const scrollToTop = function scrollToTopOfDoc() {
+  //   const distanceFromTop =
+  //     document.documentElement.scrollTop || document.body.scrollTop;
+  //   // We'll also animate that scroll with requestAnimationFrame:
+  //   // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  //   if (distanceFromTop > 0) {
+  //     window.requestAnimationFrame(scrollToTop);
+  //     // ScrollTo takes an x and a y coordinate.
+  //     // Increase the '10' value to get a smoother/slower scroll!
+  //     window.scrollTo(0, distanceFromTop - distanceFromTop / 1);
+  //   }
+  // };
+
+  // const showScrollButton = function showScrollButtonWhenBeyondWindow() {
+  //   let scrollY = window.scrollY;
+  //   if (scrollY > 350) {
+  //     scrollTopButton.className = "back-to-top show";
+  //   } else {
+  //     scrollTopButton.className = "back-to-top hidden";
+  //   }
+  // };
+
+  const coursePages = document.querySelectorAll(".course-page");
+  const overview = document.querySelector(".overview");
+  const curriculum = document.querySelector(".curriculum");
+  const overviewPage = document.querySelector(".overview-page");
+  const curriculumPage = document.querySelector(".curriculum-page");
+  const navItems = document.querySelectorAll(".course-nav-item");
+
+  // const activeToggler = function activeClassToggler(ev) {
+  //   ev.preventDefault();
+  // };
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      coursePages.forEach((page) => {
+        if (page.classList.contains("active")) {
+          page.classList.remove("active");
+        } else {
+          page.classList.add("active");
+        }
+      });
+    });
+  });
 };
-
-//  Alternative topScroll function
-// window.addEventListener("scroll", showScrollButton);
-
-// const scrollToTop = function scrollToTopOfDoc() {
-//   const distanceFromTop =
-//     document.documentElement.scrollTop || document.body.scrollTop;
-//   // We'll also animate that scroll with requestAnimationFrame:
-//   // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
-//   if (distanceFromTop > 0) {
-//     window.requestAnimationFrame(scrollToTop);
-//     // ScrollTo takes an x and a y coordinate.
-//     // Increase the '10' value to get a smoother/slower scroll!
-//     window.scrollTo(0, distanceFromTop - distanceFromTop / 1);
-//   }
-// };
-
-// const showScrollButton = function showScrollButtonWhenBeyondWindow() {
-//   let scrollY = window.scrollY;
-//   if (scrollY > 350) {
-//     scrollTopButton.className = "back-to-top show";
-//   } else {
-//     scrollTopButton.className = "back-to-top hidden";
-//   }
-// };
