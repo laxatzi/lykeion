7503b403cb9e0f58f8d412b80b861b025b50a215
laxatzi <laxatzi@yahoo.com>
2023-10-24 11:53:05 +0300

    Refactor - make subpageInteraction func work

--- JS/main.js
+++ JS/main.js
@@ -60,17 +60,35 @@ window.onload = function () {
   // };
 
   // active page code
-  function changeSubPage(p) {
-    if (p == 0) {
-      document.getElementById("overview-page").classList.toggle("active");
-    } else if (p == 1) {
-      document.getElementById("curriculum-page").classList.toggle("active");
-    } else if (p == 2) {
-      document.getElementById("tutor-nav-page").classList.toggle("active");
-    } else {
-      document.getElementById("reviews-page").classList.toggle("active");
+
+  const courseNav = document.querySelector(".course-nav");
+  const subpages = document.querySelectorAll(".subpage");
+
+  courseNav.addEventListener("click", subpageInteraction);
+  // When a child element of `buttons` is clicked
+  function subpageInteraction(e) {
+    // Check to see if its a button
+    if (e.target.matches("button")) {
+      // For every element in the `panels` node list use `classList`
+      // to remove the show class
+
+      subpages.forEach((subpage) => subpage.classList.remove("show"));
+
+      // "Destructure" the `id` from the button's data set
+      const { id } = e.target.dataset;
+
+      // Create a selector that will match the corresponding
+      // panel with that id. We're using a template string to
+      // help form the selector. Basically it says find me an element
+      // with a "panel" class which also has an id that matches the id of
+      // the button's data attribute which we just retrieved.
+      const matchId = `.subpage[id="${id}"]`;
+
+      // Select the `div` and, using classList, again add the
+      // show class
+      document.querySelector(matchId).classList.add("show");
     }
   }
 
-  // end of wrapper function
+  // end of wrapper func
 };
