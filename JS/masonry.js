f5bed0f2d686e87ea012f49dca1abea05cf5b33d
laxatzi <laxatzi@yahoo.com>
2023-11-06 13:28:56 +0200

    Add gallery images variables

--- JS/masonry.js
+++ JS/masonry.js
@@ -1,7 +1,13 @@
 const galleryFilters = document.querySelector(".gallery-filters");
+const galImage1 = document.querySelector(".gal-image:nth-child(1)");
+const galImage2 = document.querySelector(".gal-image:nth-child(2)");
+const galImage3 = document.querySelector(".gal-image:nth-child(3)");
+const galImage4 = document.querySelector(".gal-image:nth-child(4)");
+const galImage5 = document.querySelector(".gal-image:nth-child(5)");
+const galImage6 = document.querySelector(".gal-image:nth-child(6)");
 
 galleryFilters.addEventListener("click", (ev) => {
-  let galleryFilterText = ev.target.textContent.trim();
+  const galleryFilterText = ev.target.textContent.trim();
 
   if (galleryFilterText === "All") {
     console.log("All");
