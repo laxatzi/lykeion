b0faa7a71ffaacf1baf2688d4a7e43da7814253f
laxatzi <laxatzi@yahoo.com>
2023-11-06 15:07:00 +0200

    Remove subpageInteraction function

--- JS/main.js
+++ JS/main.js
@@ -61,34 +61,5 @@ window.onload = function () {
 
   // Code for selecting subPages
 
-  const courseNav = document.querySelector(".course-nav");
-  const subpages = document.querySelectorAll(".subpage");
-
-  courseNav.addEventListener("click", subpageInteraction);
-  // When a child element of `buttons` is clicked
-  function subpageInteraction(e) {
-    // Check to see if its a button
-    if (e.target.matches("button")) {
-      // For every element in the `panels` node list use `classList`
-      // to remove the active class
-
-      subpages.forEach((subpage) => subpage.classList.remove("active"));
-
-      // "Destructure" the `id` from the button's data set
-      const { id } = e.target.dataset;
-
-      // Create a selector that will match the corresponding
-      // panel with that id. We're using a template string to
-      // help form the selector. Basically it says find me an element
-      // with a "panel" class which also has an id that matches the id of
-      // the button's data attribute which we just retrieved.
-      const matchId = `.subpage[id="${id}"]`;
-
-      // Select the `div` and, using classList, again add the
-      // active class
-      document.querySelector(matchId).classList.add("active");
-    }
-  }
-
   // end of wrapper func
 };
