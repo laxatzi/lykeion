d9bc94ef7652927134d258e05d87fe8a3dca02a7
laxatzi <laxatzi@yahoo.com>
2023-11-09 16:17:07 +0200

    Remove code from main.js

--- JS/main.js
+++ JS/main.js
@@ -1,61 +1,34 @@
 // © Lambros Hatzinikolaou, 2023
 // Wrapping the code in a function and assigning it to window.onload makes sure the script is executed right after everything on the page is loaded, hence nothing remains null
 // otherwise it logs Uncaught TypeError: Cannot set property 'onclick' of null
-window.onload = function () {
-  const scrollTopButton = document.getElementById("js--back-to-top");
 
-  // When the user scrolls down 250px from the top of the document, show the button
-  window.onscroll = function () {
-    scrollFunction();
-  };
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
 
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 350 ||
-      document.documentElement.scrollTop > 350
-    ) {
-      scrollTopButton.style.opacity = 1;
-    } else {
-      scrollTopButton.style.opacity = 0;
-    }
-  }
+//  Alternative topScroll function
+// window.addEventListener("scroll", showScrollButton);
 
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-  }
+// const scrollToTop = function scrollToTopOfDoc() {
+//   const distanceFromTop =
+//     document.documentElement.scrollTop || document.body.scrollTop;
+//   // We'll also animate that scroll with requestAnimationFrame:
+//   // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+//   if (distanceFromTop > 0) {
+//     window.requestAnimationFrame(scrollToTop);
+//     // ScrollTo takes an x and a y coordinate.
+//     // Increase the '10' value to get a smoother/slower scroll!
+//     window.scrollTo(0, distanceFromTop - distanceFromTop / 1);
+//   }
+// };
 
-  scrollTopButton.addEventListener("click", topFunction);
+// const showScrollButton = function showScrollButtonWhenBeyondWindow() {
+//   let scrollY = window.scrollY;
+//   if (scrollY > 350) {
+//     scrollTopButton.className = "back-to-top show";
+//   } else {
+//     scrollTopButton.className = "back-to-top hidden";
+//   }
+// };
 
-  // Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+// Code for selecting subPages
 
-  //  Alternative topScroll function
-  // window.addEventListener("scroll", showScrollButton);
-
-  // const scrollToTop = function scrollToTopOfDoc() {
-  //   const distanceFromTop =
-  //     document.documentElement.scrollTop || document.body.scrollTop;
-  //   // We'll also animate that scroll with requestAnimationFrame:
-  //   // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
-  //   if (distanceFromTop > 0) {
-  //     window.requestAnimationFrame(scrollToTop);
-  //     // ScrollTo takes an x and a y coordinate.
-  //     // Increase the '10' value to get a smoother/slower scroll!
-  //     window.scrollTo(0, distanceFromTop - distanceFromTop / 1);
-  //   }
-  // };
-
-  // const showScrollButton = function showScrollButtonWhenBeyondWindow() {
-  //   let scrollY = window.scrollY;
-  //   if (scrollY > 350) {
-  //     scrollTopButton.className = "back-to-top show";
-  //   } else {
-  //     scrollTopButton.className = "back-to-top hidden";
-  //   }
-  // };
-
-  // Code for selecting subPages
-
-  // end of wrapper func
-};
+// end of wrapper func
